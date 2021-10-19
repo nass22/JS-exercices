2988972c5dfe43ccf018d79577147a6b9ab356f7
Samir Nacer <nacer.smr@gmail.com>
2021-10-19 15:39:21 +0200

    ex3 handling errors

--- 10-asynchronous/03-handling-errors/script.js
+++ 10-asynchronous/03-handling-errors/script.js
@@ -10,5 +10,16 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+    let btn=document.getElementById('run');
+
+    btn.addEventListener('click', () => {
+        window.lib.getPersons((error,persons) => {
+            if (error){
+                console.error(error);
+            } else {
+                console.log(persons);
+            }
+        })
+    })
+    
 })();
