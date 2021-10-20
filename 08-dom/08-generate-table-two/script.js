a4fea6f487af5494b0fae15f167a6e74e7de0462
Samir Nacer <nacer.smr@gmail.com>
2021-10-20 20:46:13 +0200

    dom ex 8 table two

--- 08-dom/08-generate-table-two/script.js
+++ 08-dom/08-generate-table-two/script.js
@@ -11,59 +11,32 @@
 
 (() => {
 
-    //A FINIR!!!
-
     var target = document.getElementById('target');
     var table = document.createElement("table");
-
+    table.setAttribute('id','table')
     target.appendChild(table);
-
-    var x = 2;
-    var y = 2;
-    var calcul = x * y;
-
-
-
-    for (f = 0; f < 10; f++) {
-
-        //création de 10 tableaux:
-        var thead = document.createElement("thead");
-        var th = document.createElement("th");
-
-        var tr = document.createElement("tr");
-        var tbody = document.createElement("tbody");
-        var row = document.createElement("tr");
-        var column = document.createElement("td");
-
-        tr.appendChild(th);
-        thead.appendChild(tr);
-        table.appendChild(thead);
-        table.appendChild(tbody);
+    var nbre=0;
+    
+    for(i=0; i<10; i++){
         
+        var tb=document.getElementById('table');
+        var titreLigne=document.createElement('tr');
+        tb.appendChild(titreLigne);
+        var titreCol=document.createElement('th');
+        titreCol.innerHTML="Table de "+(nbre+1);
+        titreLigne.appendChild(titreCol);
 
-
-        for (i = 0; i < 11; i++) {
-            var ligne = document.createElement("td");
-            row.appendChild(ligne);
-            var row = document.createElement("tr");
-            tbody.appendChild(row);
+        function table(nbre){
+            for (j=1; j<11; j++){
+                var createTr=document.createElement('tr');
+                tb.appendChild(createTr);
+                var createTd=document.createElement('td');
+                createTd.innerHTML=j+"x"+nbre+"="+nbre*j;
+                createTr.appendChild(createTd);
+            }    
         }
-
-        var td = document.getElementsByTagName("td");
-        var multiple2 = 1;
-        
-        for (j = 1; j < 11; j++) {
-            
-            var multiple1 = [j];
-            var result = multiple1 * multiple2;
-            td[j - 1].innerHTML = multiple1 + "x" + multiple2 + "=" + result;
-            
-        }
-        multiple2++;
-        console.log(td);
-        
+        nbre++;    
+        table(nbre);
     }
-
-
 
 })();
