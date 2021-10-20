02f6daf61be38f879b3a92e52f4ae2070c3c22b8
Samir Nacer <nacer.smr@gmail.com>
2021-10-20 16:01:32 +0200

    ex 4 fetch finish

--- 11-fetch/04-add/script.js
+++ 11-fetch/04-add/script.js
@@ -10,5 +10,52 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+    let btn = document.getElementById('run');
+    let btnName=document.getElementById('hero-name');
+    let btnEgo=document.getElementById('hero-alter-ego');
+    let btnPowers=document.getElementById('hero-powers');
+
+    btnName.setAttribute('required', '');
+    btnEgo.setAttribute('required', '');
+    btnPowers.setAttribute('required', '');
+
+    
+
+
+    btn.addEventListener('click', () => {
+        let inputName = document.getElementById('hero-name').value;
+        let inputEgo = document.getElementById('hero-alter-ego').value;
+        let inputPower = document.getElementById('hero-powers').value;
+        
+
+        if (inputName=="" || inputEgo=="" || inputPower==""){
+            alert("Veuillez renseigner tout les champs!");
+        } else {
+            let idHeroes;
+            fetch('http://localhost:3000/heroes')
+            .then(result => result.json())
+            .then (data => idHeroes=JSON.stringify(data.length))
+            
+            fetch('http://localhost:3000/heroes', {
+                method: "POST",
+                body: JSON.stringify({
+                    id: idHeroes,
+                    name: inputName,
+                    alterEgo: inputEgo,
+                    abilities: [inputPower]
+                }),
+                
+                headers: {
+                    "Content-type": "application/json; charset=UTF-8"
+                }
+               
+            })
+            .then (response => response.json())
+            .then (data => {
+                console.log(data)})
+        }
+        
+    })
+
+
 })();
